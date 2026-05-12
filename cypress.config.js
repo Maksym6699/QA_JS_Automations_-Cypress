c6cbc87ec0b417c3cda0e0b5ffbf610fc953dead
Maksym6699 <kozyrmaxim20@gmail.com>
2026-05-12 13:22:48 +0300

    Added fix HW_1

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  watchForFileChanges: false,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
 
   e2e: {
     setupNodeEvents(on, config) {
